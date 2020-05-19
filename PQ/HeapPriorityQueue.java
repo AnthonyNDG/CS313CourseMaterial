f671fb9962190085432fae6c4ef0dfa4628c4095
AnthonyNDG <48762898+AnthonyNDG@users.noreply.github.com>
2020-05-18 22:56:43 -0400

    Update HeapPriorityQueue.java

--- PQ/HeapPriorityQueue.java
+++ PQ/HeapPriorityQueue.java
@@ -1,89 +1,121 @@
-import java.util.Iterator;
+import java.util.ArrayList;
+import java.util.List;
 
-public class HeapPriorityQueue<K extends Comparable<K>> implements
-      PriorityQueue<K> {
-   private K data[];
-   private int size;
-   private int capacity;
+public class PQ {
+    int data[];
+    int size;
+    int capacity;
 
-   // constructors
-   public HeapPriorityQueue() {
-      capacity = 100;
-      size = 0;
-      data = (K[]) new Comparable[capacity];
-   }
+    PQ(int capacity){
+        this.capacity = capacity;
+        size = 0;
+        data = new int[capacity];
+    }
 
-   public HeapPriorityQueue(int c) {
-      capacity = c;
-      size = 0;
-      data = (K[]) new Comparable[capacity];
-   }
+    PQ(){
+        capacity = 1000;
+        size = 0;
+        data = new int[capacity];
+    }
 
-   // required priority queue methods
-   public void add(K x) throws Exception {
-      if (size >= capacity - 1)
-         throw new Exception("Priority Queue Full");
-      data[size++] = x;
-      bubbleUp(size - 1);
-   }
+    PQ(List<Integer> data) throws IllegalAccessException{
+        capacity = data.size() > 1000 ? data.size() : 1000;
+        size = data.size();
+        heapify(data);
+    }
 
-   public K removeMin() throws Exception {
-      if (size <= 0)
-         throw new Exception("Priority Queue Empty");
-      swapData(0, --size);
-      bubbleDown(0);
-      return data[size];
-   }
+    public void insert(int dahta){
+        //Insert the new data into the PQ, ensure the heap maintains heap order/shape
+        //fill in
+    	if(size == capacity ) {
+    		System.out.println("PQ is full");
+    		return;
+    	}
+    	data[size++] = dahta;
+    	bubbleUp(size-1);
+    	size++;
+    }
 
-   // auxiliary utility methods
-   private void swapData(int n, int m) {
-      K temp = data[n];
-      data[n] = data[m];
-      data[m] = temp;
-   }
+    public void remove(){
+        //Removes the root or the node with the highest priority
+    	//fill in
+        if(size<=0) {
+        	System.out.println(" The list is full ");
+        	return;
+        }
+        else {
+        	size--;
+        	data[0]=data[size];
+        	bubbleDown(0);
+        }
+    }
 
-   private void bubbleUp(int n) {
-      if (n <= 0)
-         return; // at root
-      K dn = data[n];
-      K dp = data[(n - 1) / 2]; // parent data
-      if (dn.compareTo(dp) >= 0)
-         return; // no problems
-      swapData(n, (n - 1) / 2);
-      bubbleUp((n - 1) / 2);
-   }
+    public int poll(){
+        
+    	if(size>0) return data[0];
+    	//Returns the node with the highest priority. This method should NOT remove that node
+        //fill in
+    	return -1;
+    }
+    
+    private void bubbleUp(int n) {
+        if (n <= 0)
+           return; // at root
+        int dn = data[n];
+        int dp = data[(n - 1) / 2]; // parent data
+        if (dn>=dp)
+           return; // no problems
+        swapData(n, (n - 1) / 2);
+        bubbleUp((n - 1) / 2);
+     }
 
-   public void bubbleDown(int n) {
-      if (2 * n + 1 >= size)
-         return; // at leaf
-      K dn = data[n];
-      K dl = data[2 * n + 1]; // left child
-      K dr = dl;
-      if (2 * n + 2 < size)
-         dr = data[2 * n + 2]; // right child
-      if (dn.compareTo(dl) < 0 && dn.compareTo(dr) < 0)
-         return; // no problems
-      if (dr.compareTo(dl) < 0) {
-         swapData(n, 2 * n + 2);
-         bubbleDown(2 * n + 2);
-      } else {
-         swapData(n, 2 * n + 1);
-         bubbleDown(2 * n + 1);
-      }
-   }
+    private void bubbleDown(int n) {
+        if (2 * n + 1 >= size)
+           return; // at leaf
+        int dn = data[n];
+        int dl = data[2 * n + 1]; // left child
+        int dr = dl;
+        if (2 * n + 2 < size)
+           dr = data[2 * n + 2]; // right child
+        if (dn<dl && dn<dr)
+           return; // no problems
+        if (dr<dl ) {
+           swapData(n, 2 * n + 2);
+           bubbleDown(2 * n + 2);
+        } else {
+           swapData(n, 2 * n + 1);
+           bubbleDown(2 * n + 1);
+        }
+     }
+    
+    private void swapData(int a, int b) {
+		// TODO Auto-generated method stub
+    	int temp = data[a];
+        data[a] = data[b];
+        data[b] = temp;
+	}
 
-   // heap creation
-   public void heapify(Iterator<K> x) throws Exception {
-      while (x.hasNext()) {
-         if (size + 1 == capacity)
-            break;
-         data[size++] = x.next();
-      }
-      int n = size / 2;
-      while (--n >= 0)
-         bubbleDown(n);
-      if (x.hasNext())
-         throw new Exception("Heap is Full");
-   }
+	private void heapify(List<Integer> list) throws IllegalAccessException{
+        //implement the heapify method that will build a PQ given a list of data
+		
+		data=new int[capacity];
+		size=0;
+		for(int i=0;i<list.get(i);i++) {
+			insert(list.get(i));
+		}
+		
+	}
+    
+
+    public List<Integer> toSortedList(){
+        //this method will return a list of all the integers in the priority queue in sorted order (Max Heap)
+        //this method will remove all the data from the pq
+    	List<Integer> answer = new ArrayList<Integer>();
+    	while(size>=1) {
+    		answer.add(poll());
+    		remove();
+    	}
+    	return answer;
+    }
 
 }
